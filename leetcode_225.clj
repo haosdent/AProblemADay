607a7de03bfec74e3d84323a31890d8ba631a074
haosdent <haosdent@gmail.com>
2013-12-14 20:30:42 +0800

    Fix leetcode_225.

--- leetcode_225.clj
+++ leetcode_225.clj
@@ -1,9 +1,19 @@
 (defn solution [coll]
-  (distinct coll))
+  (loop [result '()
+         coll coll]
+    (let [item (first coll)
+          coll (if (= item
+                      (first (rest coll)))
+                 (drop-while #(= item %) coll)
+                 coll)]
+      (if (nil? (first coll))
+        result
+        (recur (concat result [(first coll)])
+               (rest coll))))))
 
 (assert
  (= (solution '(1 2 2 3 4 5))
-    '(1 2 3 4 5)))
+    '(1 3 4 5)))
 
 (assert
  (= (solution '(1 2 3 4 5))
@@ -11,6 +21,6 @@
 
 (assert
  (= (solution '(1 2 3 3 4 5 5))
-    '(1 2 3 4 5)))
+    '(1 2 4)))
 
 (println "Pass!")
