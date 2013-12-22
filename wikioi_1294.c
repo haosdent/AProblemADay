aba4492ad5c847858f026f235c2d2b05f34e0caf
haosdent <haosdent@gmail.com>
2013-12-23 07:08:04 +0800

    Finish wikioi_1294.

---
+++ wikioi_1294.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXN 10
+
+int N;
+
+int history[MAXN];
+int count;
+
+void dfs(int i) {
+  int j, k;
+  if (i == N) {
+    for (j = 0; j < N; j++) {
+      printf("%d", history[j]);
+    }
+    printf("\n");
+    return;
+  }
+
+  for (k = 1; k <= N; k++) {
+    int ok = 1;
+    for (j = 0; j < i; j++) {
+      if (history[j] == k) {
+        ok = 0;
+      }
+    }
+    if (ok) {
+      history[i] = k;
+      dfs(i + 1);
+    }
+  }
+}
+
+int main() {
+  scanf("%d", &N);
+  dfs(0);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
