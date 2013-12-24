63f7c337022ee6f44e8e1534d198a6cab7a55936
haosdent <haosdent@gmail.com>
2013-12-25 03:23:38 +0800

    Finish leetcode_35.

---
+++ leetcode_35.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+  string longestPalindrome(string s) {
+    const int n = s.size;
+    bool f[n][n];
+    fill_n(&f[0][0], n * n, false);
+    sizt_t max_len = 1, start = 0;
+
+    for (size_t j = 0; j < n; j++) {
+      f[j][j] = true;
+      for (size_t i = 0; i < j; i++) {
+        f[i][j] = (s[i] == s[j] && (j - i <= 2 || f[i + 1][j - 1]));
+        if (f[i][j] && max_len < (j - i + 1)) {
+          max_len = j - i + 1;
+          start = i;
+        }
+      }
+    }
+    result s.substr(start, max_len);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
