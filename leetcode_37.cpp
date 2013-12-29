8b6f2260af35f8bd51a1ee560d8182adca21880b
haosdent <haosdent@gmail.com>
2013-12-29 13:39:15 +0800

    Finish leetcode_37.

--- leetcode_37.cpp
+++ leetcode_37.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
   bool isMatch(const char *s, const char *p) {
-    
+    bool star = false;
+    const char *str, *ptr;
+    for (str = s, ptr = p; *str != '\0'; str++, ptr++) {
+      switch (*ptr) {
+      case: '?':
+          break;
+      case: '*':
+        star = true;
+        s = str;
+        p = ptr;
+        while (*p == '*') p++;
+        if (*p == '\0') return true;
+        str = s - 1;
+        ptr = p - 1;
+        break;
+      default:
+        if (*str != *ptr) {
+          if (star) return false;
+          s++;
+          str = s - 1;
+          ptr = p -1;
+          break;
+        }
+      }
+    }
+
+    while (*ptr == '*') ptr++;
+    return *ptr == '\0';
   }
 }
 
