6fee21adcc2e45119db08c5319db0521cd4b912b
haosdent <haosdent@gmail.com>
2014-01-15 10:12:47 +0800

    Finish leetcode_5112.

---
+++ leetcode_5112.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+      if (root == null) return;
+
+      TreeLinkNode next = new TreeLinkNode(-1);
+      for (TreeLinkNode curr = root, prev = next; curr != null; curr = curr.next) {
+        if (curr.left != null) {
+          prev.next = curr.left;
+          prev = prev.next;
+        }
+        if (curr.right != null) {
+          prev.next = curr.right;
+          prev = prev.next;
+        }
+      }
+      connect(next.next);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
