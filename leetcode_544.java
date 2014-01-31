438090f7e96dd9285dae83a4d7812f1ec3185e04
haosdent <haosdent@gmail.com>
2014-01-31 16:31:44 +0800

    Finish leetcode_544.

--- leetcode_544.java
+++ leetcode_544.java
@@ -8,7 +8,22 @@
  * }
  */
 public class Solution {
-  public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
-    
+  public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {    
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    ArrayList<Integer> cur = new ArrayList<Integer>();
+    pathSum(root, sum, cur, result);
+    return result;
+  }
+  public void pathSum(TreeNode root, int sum, ArrayList<Integer> cur, ArrayList<ArrayList<Integer>> result) {
+    if (root == null) return;
+    cur.add(root.val);
+    if (root.left == null && root.right == null) {
+      if (sum == root.val) {
+        result.add(new ArrayList<Integer>(cur));
+      }
+    }
+    pathSum(root.left, sum - root.val, cur, result);
+    pathSum(root.right, sum - root.val, cur, result);
+    cur.remove(cur.size() - 1);
   }
 }
