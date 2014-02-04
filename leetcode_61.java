b593ea12f08721c11b97ed51d40dda3e23f89f13
haosdent <haosdent@gmail.com>
2014-02-04 23:17:06 +0800

    Finish leetcode_61.

---
+++ leetcode_61.java
@@ -0,0 +1,21 @@
+public class Solution {
+  public void merge(int A[], int m, int B[], int n) {
+    int ia = m - 1, ib = n - 1, icur = m + n - 1;
+    for (; ia >= 0 && ib >= 0; ) {
+      A[icur--] = A[ia] > B[ib] ? A[ia--] : B[ib--];
+    }
+    for (; ib >= 0; ) {
+      A[icur--] = B[ib--];
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
