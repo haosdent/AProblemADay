2de80c7d17e422103ed098d200f856dc0ef9efb4
haosdent <haosdent@gmail.com>
2014-02-23 23:55:40 +0800

    Finish leetcode_86.

--- leetcode_86.java
+++ leetcode_86.java
@@ -1,4 +1,35 @@
 public class Solution {
-  public ArrayList<String> letterCombinations(String digits) {    
+  final String[] keys = {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+  public ArrayList<String> letterCombinations(String digits) {
+    ArrayList<String> result = new ArrayList<String>();
+    dfs(digits, 0, "", result);
+    return result;
+  }
+  public void dfs(String digits, int cur, String path, ArrayList<String> result) {
+    if (cur == digits.length()) {
+      result.add(path);
+      return;
+    }
+    for (char k : keys[digits.charAt(cur) - '0'].toCharArray()) {
+      dfs(digits, cur + 1, path + k, result);
+    }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
