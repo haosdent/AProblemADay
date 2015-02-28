fc1708298c86d878a38206c9877f6f4f81168fbc
haosdent <haosdent@gmail.com>
2015-02-28 22:16:38 +0800

    Update leetcode_137

--- leetcode_137.py
+++ leetcode_137.py
@@ -1,4 +1,7 @@
 class Solution:
     # @return a boolean
     def isScramble(self, s1, s2):
-        
+        l = len(s1)
+        if len(s1) != len(s2):
+            return False
+        f = [[[False for z in xrange(l)] for y in xrange(l)] for x in xrange(l + 1)]
