c55dd248bb90f5b86b53420db56a37226ff8d484
haosdent <haosdent@gmail.com>
2015-03-01 22:16:38 +0800

    Update leetcode_137

--- leetcode_137.py
+++ leetcode_137.py
@@ -5,3 +5,11 @@ class Solution:
         if len(s1) != len(s2):
             return False
         f = [[[False for z in xrange(l)] for y in xrange(l)] for x in xrange(l + 1)]
+
+        for i in xrange(l):
+            for j in xrange(l):
+                f[1][i][j] = s1[i] == s2[j]
+
+        for i in xrange(1, l + 1):
+            for j in xrange(l - i):
+                for k in xrange()
