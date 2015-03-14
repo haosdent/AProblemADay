e4780980ff6f603e41eecd2337b1649eba3d7563
haosdent <haosdent@gmail.com>
2015-03-14 12:03:06 +0800

    Add leetcode_1311.py

--- leetcode_1311.py
+++ leetcode_1311.py
@@ -1,15 +1,21 @@
 class Solution:
     def numDistinct(self, s, t):
-        f = [0 for x in xrange(len(t) + 1)]
-        f[0] = 1
-        for i in xrange(len(s)):
-            for j in xrange(len(t) - 1, -1, -1):
-                print "S[%d]: %s, T[%d]: %s, f[%d]: %d" % (i, s[i], j, t[j], j, f[j])
-                if s[i] == t[j]:
-                    f[j + 1] += f[j]
+        sl = len(s)
+        tl = len(t)
+        f = [[0 for x in xrange(tl + 1)] for y in xrange(sl + 1)]
+        for i in xrange(tl + 1):
+            f[0][i] = 0
+        for i in xrange(sl + 1):
+            f[i][0] = 1
 
-        return f[len(t)]
+        for i in xrange(1, sl + 1):
+            for j in xrange(1, min(i + 1, tl + 1)):
+                if s[i - 1] == t[j - 1]:
+                    f[i][j] = f[i - 1][j - 1] + f[i - 1][j]
+                else:
+                    f[i][j] = f[i - 1][j]
+        return f[sl][tl]
 
 s = Solution()
-#print s.numDistinct("rabbbit", "rabbit")
-print s.numDistinct("ab", "cd")
+print s.numDistinct("rabbbit", "rabbit")
+print s.numDistinct("abb", "ab")
