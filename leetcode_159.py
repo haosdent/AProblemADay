10e7ca4d61c145d00c8d157b8e58d6b854a3d460
haosdent <haosdent@gmail.com>
2015-03-17 01:58:01 +0800

    Finish leetcode_159.

---
+++ leetcode_159.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getRow(self, rowIndex):
+        if rowIndex <= 0:
+            return []
+        result_row = [1]
+        for i in xrange(1, rowIndex + 1):
+            result_row.insert(0, 0)
+            result_row.append(0)
+            now_row = []
+            for j in xrange(len(result_row) - 1):
+                now_row.append(result_row[j] + result_row[j + 1])
+            result_row = now_row
+        return result_row
+
+
+s = Solution()
+print s.getRow(3)
