897ed4e6716b27b8c96325b308e7f6d6361bf90f
haosdent <haosdent@gmail.com>
2015-03-18 02:14:03 +0800

    Add leetcode_1511.py

---
+++ leetcode_1511.py
@@ -0,0 +1,41 @@
+class Solution:
+    # @return a list of lists of integer
+    def generateMatrix(self, n):
+        matrix = [[0 for x in xrange(n)] for y in xrange(n)]
+        beginX = 0
+        beginY = 0
+        endY = len(matrix) - 1
+        if endY < 0:
+            return result
+        endX = len(matrix[0]) - 1
+        num = 0
+        while True:
+            for i in xrange(beginX, endX + 1):
+                num += 1
+                matrix[beginY][i] = num
+            beginY += 1
+            if beginY > endY:
+                break
+            for i in xrange(beginY, endY + 1):
+                num += 1
+                matrix[i][endX] = num
+            endX -= 1
+            if beginX > endX:
+                break
+            for i in xrange(endX, beginX - 1, -1):
+                num += 1
+                matrix[endY][i] = num
+            endY -= 1
+            if beginY > endY:
+                break
+            for i in xrange(endY, beginY - 1, -1):
+                num += 1
+                matrix[i][beginX] = num
+            beginX += 1
+            if beginX > endX:
+                break
+        return matrix
+
+
+s = Solution()
+print s.generateMatrix(3)
