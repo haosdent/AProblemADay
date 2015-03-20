e580f82061d69ea770cfea006b6737cb9ac43617
haosdent <haosdent@gmail.com>
2015-03-21 02:15:36 +0800

    Add leetcode_1512.

--- leetcode_1512.py
+++ leetcode_1512.py
@@ -1,5 +1,18 @@
 class Solution:
     # @return a string
     def convert(self, s, nRows):
+        sl = len(s)
+        if nRows <= 1 or sl <= 1:
+            return s
+        result = ""
         for i in xrange(nRows):
-            for j in xrange():
+            for j in xrange(i, sl, nRows * 2 - 2):
+                result += s[j]
+                tmp_i = j + (nRows - i - 1) * 2
+                if j < tmp_i and tmp_i < j + nRows * 2 - 2 and tmp_i < sl:
+                    result += s[tmp_i]
+        return result
+
+
+s = Solution()
+print s.convert("PAYPALISHIRING", 3)
