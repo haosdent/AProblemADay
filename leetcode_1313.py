e2e2b6bc97ab4be3c39a84054f9991cc964557a4
haosdent <haosdent@gmail.com>
2015-03-24 01:40:38 +0800

    Finish leetcode_1313.py

---
+++ leetcode_1313.py
@@ -0,0 +1,44 @@
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a list of strings
+    def wordBreak(self, s, dic):
+        f = [False for x in xrange(len(s) + 1)]
+        f[0] = True
+        prev_map = {}
+        for i in xrange(len(s) + 1):
+            for j in xrange(i):
+                if f[j] and s[j:i] in dic:
+                    f[i] = True
+                    if i not in prev_map:
+                        prev_map[i] = []
+                    prev_map[i].append(j)
+        result = []
+        self.findPath([len(s)], prev_map, s, result)
+        return result
+
+    def findPath(self, path, prev_map, s, result):
+        path_last = path[len(path) - 1]
+        if path_last == 0:
+            tmp_pos = 0
+            tmp_sen = ''
+            for i in reversed(path):
+                if i == 0:
+                    continue
+                tmp_sen += s[tmp_pos:i]
+                tmp_sen += ' '
+                tmp_pos = i
+            if tmp_sen[len(tmp_sen) - 1] == ' ':
+                tmp_sen = tmp_sen[:len(tmp_sen) - 1]
+            result.append(tmp_sen)
+            return
+        if path_last in prev_map:
+            prev = prev_map[path_last]
+            for i in prev:
+                path.append(i)
+                self.findPath(path, prev_map, s, result)
+                path.pop()
+
+
+s = Solution()
+print s.wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"])
